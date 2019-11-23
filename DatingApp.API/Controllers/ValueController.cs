643c3cf2cd05100b126051ddc49504e9f553758b
pollpbl <pietrzak.p@gmail.com>
2019-11-23 10:04:43 +0100

    first commit

---
+++ DatingApp.API/Controllers/ValueController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.DatingApp;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }       
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+             var value = await _context.Values.FirstOrDefaultAsync(o=>o.Id == id);
+
+             return Ok(value);
+        }
+
+        
+
+   
+    }
+}
