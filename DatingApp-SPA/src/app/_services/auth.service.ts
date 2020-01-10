dbfc55541d6d98443f3dbafd9a5836b712512805
pollpbl <pietrzak.p@gmail.com>
2020-01-10 16:00:14 +0100

    End of section 4 (register new user)

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -5,6 +5,7 @@ import {map} from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
@@ -20,7 +21,11 @@ constructor(private http: HttpClient) { }
           localStorage.setItem('token', user.token);
         }
       })
-    )
+    );
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
 }
