c107c84e3877566db117eca05750611636ac0de2
pollpbl <pietrzak.p@gmail.com>
2020-02-09 10:25:53 +0100

    Added filtering

--- DatingApp.API/Dtos/UserForListDto.cs
+++ DatingApp.API/Dtos/UserForListDto.cs
@@ -16,6 +16,8 @@ namespace DatingApp.API.Dtos
 
         public DateTime Created { get; set; }
 
+        public DateTime LastActive { get; set; }
+
         public string City { get; set; }
 
         public string Country { get; set; }
