50750ccc9155f76b958daf6e253ca895857a0e90
pollpbl <pietrzak.p@gmail.com>
2020-03-08 21:31:48 +0100

    Addad Lazy loading proxies form .net core

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -37,13 +37,13 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
             return user;
         }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
+            var users = _context.Users.OrderByDescending(u => u.LastActive).AsQueryable();
 
             users = users.Where(u => u.Id != userParams.UserId);
 
@@ -82,10 +82,7 @@ namespace DatingApp.API.Data
         }
 
         private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers) {
-            var user = await _context.Users
-            .Include(x=> x.Likers)
-            .Include(x=>x.Likees)
-            .FirstOrDefaultAsync(u=>u.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync(u=>u.Id == id);
 
             if (likers) {
                 return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
@@ -116,10 +113,7 @@ namespace DatingApp.API.Data
 
         public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
-            var messages = _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .AsQueryable();
+            var messages = _context.Messages.AsQueryable();
 
             switch (messageParams.MessageContainer) {
                 case "Inbox":
@@ -143,8 +137,6 @@ namespace DatingApp.API.Data
         public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
             var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(m => m.RecipientId == userId && m.RecipientDeleted == false && m.SenderId == recipientId 
                     || m.RecipientId == recipientId && m.SenderId == userId && m.SenderDeleted == false)
                 .OrderByDescending(m => m.MessageSent)
